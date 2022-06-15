fea3872020862590c1dfae5e0f71143f5d8b268e
Tatiana Kazakova <test.kazakova@gmail.com>
2022-06-15 13:17:16 +0300

    added properties and tests

--- src/test/java/tests/HomeWorkTests.java
+++ src/test/java/tests/HomeWorkTests.java
@@ -9,11 +9,12 @@ import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.back;
+import static io.qameta.allure.Allure.step;
 
 
 @Tag("android")
 public class HomeWorkTests extends TestBase {
-    @Disabled
+
     @DisplayName("Проверка заголовка статьи, что он соответствует запросу поиска")
     @Test
     void homeWorkWikiTest() {
@@ -26,35 +27,47 @@ public class HomeWorkTests extends TestBase {
 
     }
 
+    @DisplayName("Проверка капчи при заполнении формы регистрации")
     @Test
     void homeWorkWikiCaptchaTest() {
         back();
-        $(AppiumBy.xpath("//android.widget.TextView[@text='Edits']")).click();
-        $(AppiumBy.id("org.wikipedia.alpha:id/positiveButton")).click();
-        $(AppiumBy.xpath("//android.widget.TextView[@text='Create an account']"))
-                .shouldHave(Condition.text("Create an account"));
-        $(AppiumBy.xpath("//android.widget.EditText[@text='Username']")).click();
-        $(AppiumBy.xpath("//android.widget.EditText[@text='Username']"))
-                .sendKeys("QaGuruHello");
+        step("Перейти к форме регистрации", ()-> {
+            $(AppiumBy.xpath("//android.widget.TextView[@text='Edits']")).click();
+            $(AppiumBy.id("org.wikipedia.alpha:id/positiveButton")).click();
+        }
+        );
+        step("Убедиться, что открылась форма Регистрации", ()->{
+            $(AppiumBy.xpath("//android.widget.TextView[@text='Create an account']"))
+                    .shouldHave(Condition.text("Create an account"));
+        });
 
-        $(AppiumBy.xpath("//android.widget.EditText[@text='Password']")).click();
-        $(AppiumBy.xpath("//android.widget.EditText[@text='Password']"))
-                .sendKeys("12345678");
+        step("Заполнить форму Регистрации", ()->{
+            $(AppiumBy.xpath("//android.widget.EditText[@text='Username']")).click();
+            $(AppiumBy.xpath("//android.widget.EditText[@text='Username']"))
+                    .sendKeys("QaGuruHello");
 
-        $(AppiumBy.xpath("//android.widget.EditText[@text='Repeat password']")).click();
-        $(AppiumBy.xpath("//android.widget.EditText[@text='Repeat password']"))
-                .sendKeys("12345678");
+            $(AppiumBy.xpath("//android.widget.EditText[@text='Password']")).click();
+            $(AppiumBy.xpath("//android.widget.EditText[@text='Password']"))
+                    .sendKeys("12345678");
 
-        $(AppiumBy.xpath("//android.widget.EditText[@text='Email (Optional)']")).click();
-        $(AppiumBy.xpath("//android.widget.EditText[@text='Email (Optional)']"))
-                .sendKeys("qaGurur123@google.com");
+            $(AppiumBy.xpath("//android.widget.EditText[@text='Repeat password']")).click();
+            $(AppiumBy.xpath("//android.widget.EditText[@text='Repeat password']"))
+                    .sendKeys("12345678");
 
-        $(AppiumBy.xpath("//android.widget.Button[@text='NEXT']")).click();
+            $(AppiumBy.xpath("//android.widget.EditText[@text='Email (Optional)']")).click();
+            $(AppiumBy.xpath("//android.widget.EditText[@text='Email (Optional)']"))
+                    .sendKeys("qaGurur123@google.com");
+        });
 
-        $(AppiumBy.id("org.wikipedia.alpha:id/request_account_text"))
-                .shouldHave(Condition.text("Can't see the image? Request an account"));
-        $(AppiumBy.id("org.wikipedia.alpha:id/captcha_image")).shouldBe(Condition.visible);
-        $(AppiumBy.id("org.wikipedia.alpha:id/captcha_submit_button")).shouldBe(Condition.visible);
+        step("Нажать на кнопку 'Next'", ()->{
+            $(AppiumBy.xpath("//android.widget.Button[@text='NEXT']")).click();
+        });
+        step("Проверка, что капча и ее отрибуты отображаются корректно",()->{
+            $(AppiumBy.id("org.wikipedia.alpha:id/request_account_text"))
+                    .shouldHave(Condition.text("Can't see the image? Request an account"));
+            $(AppiumBy.id("org.wikipedia.alpha:id/captcha_image")).shouldBe(Condition.visible);
+            $(AppiumBy.id("org.wikipedia.alpha:id/captcha_submit_button")).shouldBe(Condition.visible);
+        });
 
     }
 }
